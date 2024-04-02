42fa63be64585c5af3316a96f916ad01fb06713c
sellylima <vanessa.limanasc@gmail.com>
2024-04-01 22:31:18 -0300

    alteração estrutura html e inserção imagens

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,8 @@
 // Para incluir los diferentes sets de cartas podemos _importar_ el archivo
 // JavasSript que contenga el `export` correspondiente...
 //
-// import pokemon from '../data/pokemon/pokemon.js';
-// console.log(pokemon);
+import jogos from '../data/jogos/jogos.js';
+console.log(jogos);
 //
 // O alternativamente podríamos cargar el JSON de forma asíncrona usando
 // `fetch` en el momento que consideremos necesario.
@@ -15,12 +15,21 @@
 //
 
 const App = () => {
-  const el = document.createElement('div');
+  const el = document.createElement("div");
 
-  el.className = 'App';
-  el.textContent = 'Hola mundo!';
-
+  el.className = "App";
+  el.innerHTML = `<main>
+  <div>
+    <h1 class="titulo">Memory Game</h1>
+    <p class="subtitulo"> Desafie seus amigos, teste sua concentração e
+      agilidade com o jogo da memória </p>
+  </div>
+  <form class="form_login">
+    <button type="submit" id="form_button" disabled>Jogar Novamente</button>
+  </form>
+ `;
   return el;
 };
+
 
 export default App;
---
+++ src/data/jogos/jogos.js
@@ -0,0 +1,10 @@
+export default {
+  name: 'Jogos',
+  items: [
+    { id: 'amongUs', image: '../img-amongUs.png' },
+    { id: 'fortnite', image: '../img-fortnite.png' },
+    { id: 'minecraft', image: '../img-minecraft.png' },
+    { id: 'roblox', image: '../img-roblox.png' },
+    { id: 'stumbleGuys', image: '../img-stumbleGuys.png' },
+  ],
+};
