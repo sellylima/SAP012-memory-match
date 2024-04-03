db7a4313404695c5310776d98f9fa947a5812c0c
sellylima <vanessa.limanasc@gmail.com>
2024-04-03 15:22:55 -0300

    criado função duplica cartas

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,24 @@
-//
-// Para incluir los diferentes sets de cartas podemos _importar_ el archivo
-// JavasSript que contenga el `export` correspondiente...
-//
-import jogos from '../data/jogos/jogos.js';
-console.log(jogos);
-//
-// O alternativamente podríamos cargar el JSON de forma asíncrona usando
-// `fetch` en el momento que consideremos necesario.
-//
-// fetch('./data/pokemon/pokemon.json')
-//   .then(resp => resp.json())
-//   .then(console.log)
-//   .catch(console.error);
-//
+import dataJogo from "../data/jogos/dataJogo.js";
+console.log(dataJogo);
 
+//duplica cartas
+const duplicaCartas = () => {
+  const duplicaJogo = dataJogo.items.concat(dataJogo.items); //metodo concatenação
+  console.log(duplicaJogo);
+};
+
+//embaralha as cartas
+// const embaralhaCartas = (cards) => {
+//   for(let i = dataJogo.length - 1; i > 0; i--) {
+    
+//   }
+// };
+
+//verificar se as cartas são iguais
+//desabilitar as cartas se não iguais
+//desvirar as cartas se não forem iguais
+
+//  HTML
 const App = () => {
   const el = document.createElement("div");
 
@@ -24,12 +29,19 @@ const App = () => {
     <p class="subtitulo"> Desafie seus amigos, teste sua concentração e
       agilidade com o jogo da memória </p>
   </div>
+  <div class="container">
+  <div class="cards">
+    <div class="imagem frente"></div> 
+    <div class="imagem verso"></div>
+  </div>
+</div>
   <form class="form_login">
     <button type="submit" id="form_button" disabled>Jogar Novamente</button>
   </form>
  `;
+
+  duplicaCartas();
   return el;
 };
-
 
 export default App;
--- src/main.js
+++ src/main.js
@@ -1,45 +1,4 @@
 import App from "./components/App.js";
 
-document.getElementById("root").appendChild(App());
+document.getElementById("root").appendChild(App()); //usa o metodo .appendChild para criar um filho do id "root" e chamar atraves da função App, criado no App.js
 
-
-
-
-
-
-
-
-// // FUNÇÃO PARA HABILITAR E DESABILITAR O BOTAO
-// const inputLogin = document.getElementById("form_input");
-// const buttonLogin = document.getElementById("form_button");
-
-// const validandoInput = (event) => {
-//   if (event.target.value.length > 3) {
-//     // fiz uma condicional passando o target trouxe o valor que ta sendo digitado no nosso input  e pegar o num. de caracteres através do length
-//     buttonLogin.removeAttribute("disabled");
-//   } else {
-//     buttonLogin.setAttribute("disabled", "");
-//   }
-//   console.log(event.target.value);
-// };
-
-// inputLogin.addEventListener("input", validandoInput);
-
-// //FUNÇÃO PARA ENVIAR DADOS FORMULARIO
-
-// const formLogin = document.querySelector('form_login');
-
-// const envioDadosForm = (event) => {
-    
-
-// }
-
-//  formLogin.addEventListener('submit', envioDadosForm);
-
-
-
-
-// length - quantidade de caracteres
-// target - obtem o elemento onde o evento ocorreu
-// setAttribute - coloca um atributo (precisa passar dois parametros)
-// preventDefault - previne/bloqueia o comportamento de recarregamento padrao de uma pagina
