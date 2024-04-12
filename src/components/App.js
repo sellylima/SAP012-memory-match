e9b980e7738fe2c66f1a5fe3b125a4b7d783ae93
sellylima <vanessa.limanasc@gmail.com>
2024-04-12 10:30:50 -0300

    inclusão de funções e estilos css

--- src/components/App.js
+++ src/components/App.js
@@ -1,47 +1,100 @@
-import dataJogo from "../data/jogos/dataJogo.js";
-console.log(dataJogo);
-
-//duplica cartas
-const duplicaCartas = () => {
-  const duplicaJogo = dataJogo.items.concat(dataJogo.items); //metodo concatenação
-  console.log(duplicaJogo);
-};
-
-//embaralha as cartas
-// const embaralhaCartas = (cards) => {
-//   for(let i = dataJogo.length - 1; i > 0; i--) {
-    
-//   }
-// };
-
-//verificar se as cartas são iguais
-//desabilitar as cartas se não iguais
-//desvirar as cartas se não forem iguais
+import game from "../data/game/game.js";
 
 //  HTML
 const App = () => {
   const el = document.createElement("div");
 
+  const arrayCartas = shuffleCards(duplicaCartas());
+
   el.className = "App";
-  el.innerHTML = `<main>
-  <div>
+  el.innerHTML = `<div>
+  <div class="banner">
     <h1 class="titulo">Memory Game</h1>
     <p class="subtitulo"> Desafie seus amigos, teste sua concentração e
       agilidade com o jogo da memória </p>
   </div>
-  <div class="container">
-  <div class="cards">
-    <div class="imagem frente"></div> 
-    <div class="imagem verso"></div>
+  <div class="card__container">
   </div>
-</div>
+  </div>
   <form class="form_login">
     <button type="submit" id="form_button" disabled>Jogar Novamente</button>
-  </form>
+  </form> 
+  </div>
  `;
 
-  duplicaCartas();
+  const cardContainer = el.querySelector(".card__container");
+
+  arrayCartas.forEach((carta) => {
+    cardContainer.innerHTML += `
+    <div class="card"  >
+    <div class="face  front"><img src="img/${carta.image}.png"></div>
+    <div class="face  back" data-nome-jogo="${carta.image}"><img src="img/verso-card.png"></div>
+  </div>
+    `;
+  });
+
+  const nodeListCard = el.querySelectorAll(".card");
+
+  nodeListCard.forEach((carta) => {
+    carta.addEventListener("click", flipCard);
+  });
+
   return el;
+};
+
+let firstCard = ""; //guarda a informação da primeira carta
+let secondCard = ""; //guarda a informação da segunda carta
+
+//DUPLICA CARTAS
+const duplicaCartas = () => {
+  const duplicaJogo = game.concat(game); //metodo concatenação
+  return duplicaJogo;
+};
+
+//EMBARALHAR CARTAS
+const shuffleCards = (cartasDuplicadas) => {
+  const shuffle = cartasDuplicadas.sort(() => Math.random() - 0.5); //função que retorna um numero aleatório
+  //   console.log(shuffle);
+  return shuffle;
+};
+
+//COMPARA SE AS CARTAS SÃO IGUAIS
+const checkCards = () => {
+  const firstCardJogo = firstCard.getAttribute("data-nome-jogo");
+  const secondCardJogo = secondCard.getAttribute("data-nome-jogo");
+  console.log(firstCardJogo);
+  console.log(secondCardJogo);
+  console.log(firstCard);
+  console.log(secondCard);
+  if (firstCardJogo === secondCardJogo) {
+    //COLOCAR ALGO AQUI
+  } else {
+    //escondendo as cartas reveladas caso a pessoa errar
+    setTimeout(() => {
+      firstCard.classList.remove("flip__card");
+      secondCard.classList.remove("flip__card");
+    }, "500");
+  }
+};
+
+//REVELA E VERIFICA CARTA
+const flipCard = ({ target }) => {
+  // target.parentNode.classList.add("flip__card"); //o pai encontrado no parent node foi (card) e adicinado uma classe flip__card
+
+  //target revela o "alvo/elemento" que esta sendo clicado
+  if (target.parentNode.className.includes("flip__card")) {
+    return;
+  }
+
+  //se o item já foi clicado, não vai fazer nada
+  if (firstCard === "") {
+    target.parentNode.classList.add("flip__card");
+    firstCard = target.parentNode;
+  } else if (secondCard === "") {
+    target.parentNode.classList.add("flip__card");
+    secondCard = target.parentNode;
+    checkCards();
+  }
 };
 
 export default App;
---
+++ src/data/game/game.js
@@ -0,0 +1,10 @@
+export default 
+  [
+    { id: "amongUs", image: "img-amongUs" },
+    { id: "fortnite", image: "img-fortnite" },
+    { id: "minecraft", image: "img-minecraft" },
+    { id: "roblox", image: "img-roblox" },
+    { id: "stumbleGuys", image: "img-stumbleGuys" }
+  ]
+
+
